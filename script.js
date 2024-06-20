01e222b954a9396d60a465af7f6f6285d618d0a1
Michelle Guo <runbo.guo.17@gmail.com>
2024-06-20 16:42:40 -0400

    adjusted some images

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // --------------- VARIABLES ------------------
 
 navbarShift = "-80px";
-
+let slideIndex = 0;
 
 // --------------- FUNCTIONS ------------------
 function dropdown() {
@@ -65,5 +65,24 @@ function scrollSect3() {
 
 window.addEventListener("scroll", reveal);
 
+//----Carousel------------------
+
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+
+//---------------------------
+
+
 //Run these on page load
 reveal();
+showSlides();
