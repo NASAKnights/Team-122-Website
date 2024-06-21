3f757f1f73013bb55a9b7d28b47c7411f4ef8920
Michelle Guo <runbo.guo.17@gmail.com>
2024-06-20 21:16:44 -0400

    added 2024 section, needs to fill in

--- script.js
+++ script.js
@@ -65,24 +65,9 @@ function scrollSect3() {
 
 window.addEventListener("scroll", reveal);
 
-//----Carousel------------------
-
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
-}
 
 //---------------------------
 
 
 //Run these on page load
 reveal();
-showSlides();
